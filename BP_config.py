c3b532f27b8eca796f620133de63c13aebba4a1d
Blanca Pina <b.pina.pinon@gmail.com>
2018-08-28 09:46:30 -0700

    remove tokens/API Key

--- BP_config.py
+++ BP_config.py
@@ -1,12 +1,8 @@
 # Twitter API Keys
-consumer_key = "YkOmJe2k9JxYISNRnTz7RSlO2"
-# "Ed4RNulN1lp7AbOooHa9STCoU"
+consumer_key = "key"
 
-consumer_secret = "pHBIHD1oNbC9xvM1LassInbg3kooTkMn2TKasiQoldfiRA3FVq"
-# "P7cUJlmJZq0VaCY0Jg7COliwQqzK0qYEyUF9Y0idx4ujb3ZlW5"
+consumer_secret = "key"
 
-access_token = "1047178226-2A9p0JITXzAWZZ56ERX6V2hPEU2L2gykaQkruIO"
-# "839621358724198402-dzdOsx2WWHrSuBwyNUiqSEnTivHozAZ"
+access_token = "token"
 
-access_token_secret = "5DNsMglyI2yVpZVqFkjJDX3MD5MDBAdcndjJfPg14AxWn"
-# "dCZ80uNRbFDjxdU2EckmNiSckdoATach6Q8zb7YYYE5ER"
+access_token_secret = "token"
